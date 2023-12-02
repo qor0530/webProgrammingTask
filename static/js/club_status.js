b08eef6d963bd5b7b1ad8fdeabdafdabc0dcdef5
Paik JongHwa <qorwhdghk12@gmail.com>
2023-12-02 20:23:52 +0900

    club_status html, css 끝

---
+++ static/js/club_status.js
@@ -0,0 +1,17 @@
+document.getElementById("article").innerHTML += `<div class="content-box">
+<img src="{% static 'image/content-image.png'%}" alt="" />
+<div class="content">
+  <div class="content-title">멋장이사자처럼</div>
+  <div class="content-description">
+    경상국립대학교 가좌 캠퍼스 유일 코딩 동아리입니다. 코딩하는 것과
+    무언가 함께 만드는걸 좋아하는 회원이라면 누구나 즐길 수 있도록
+    자유로운 활동을 지향하고 있습니다.
+  </div>
+  <div class="crossline"></div>
+  <div class="descrition">
+    <div class="writer">백종화</div>
+    <div class="date">2023-12-02</div>
+  </div>
+</div>
+</div>`
+ 
--- web_site/settings.py
+++ web_site/settings.py
@@ -115,8 +115,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
